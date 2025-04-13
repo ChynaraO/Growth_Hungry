fc857de8d77ee42b0b3bdb949a55e02dbb4184f3
ChynaraO <omurzakova_chyna@mail.ru>
2025-04-13 15:37:12 -0500

    CS week 4

---
+++ src/ComputerSystemsAndIndustrialProgramming/W4/Main.java
@@ -0,0 +1,11 @@
+package ComputerSystemsAndIndustrialProgramming.W4;
+
+public class Main {
+    public static void main(String[] args) {
+        double multiplied = Calculator.multiply(3.14, 0.3, 7);
+        System.out.println("Multiplying answer is: " + multiplied);
+
+        double sum = Calculator.add(3,5,6,8,9.0);
+        System.out.println("Sum id: " + sum);
+    }
+}
