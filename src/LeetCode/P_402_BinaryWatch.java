bae935879420878ec9e6f07073ca90fbb7d1e4b1
ChynaraO <omurzakova_chyna@mail.ru>
2025-09-19 10:03:25 -0500

    Leetcode: 402

---
+++ src/LeetCode/P_402_BinaryWatch.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P_402_BinaryWatch {
+    public static List<String> readBinaryWatch(int turnedOn) {
+       List<String> result = new ArrayList<>();
+       for( int h = 0; h<12; h++){
+           for(int m = 0; m<60; m++){
+               if(Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                   result.add(String.format("%d:%02d", h, m));
+               }
+           }
+       }
+       return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(readBinaryWatch(3));
+    }
+}
