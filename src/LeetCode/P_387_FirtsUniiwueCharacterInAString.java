c61a7984df5bf6041db785673d89b698c2b70c24
ChynaraO <omurzakova_chyna@mail.ru>
2025-10-30 22:05:49 -0500

    Leetcode: 387

--- src/LeetCode/P_387_FirtsUniiwueCharacterInAString.java
+++ src/LeetCode/P_387_FirtsUniiwueCharacterInAString.java
@@ -1,5 +1,8 @@
 package LeetCode;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class P_387_FirtsUniiwueCharacterInAString {
     /*
     Given a string s, find the first non-repeating character in it and return its index.
@@ -32,5 +35,24 @@ The character 'l' at index 0 is the first character that does not occur at any o
                 return i;
             }
         } return -1;
+    }
+    public int firstUniqChar1(String s) {
+        Map<Character, Integer> mapN = new LinkedHashMap<>();
+        //l:1 , e:2; t:1
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            mapN.put(c,   mapN.getOrDefault(c, 0)+1);
+        }
+        //l : 2
+        //e : 2
+        //t : 1
+        //b : 1
+
+        for(Map.Entry<Character, Integer> entity : mapN.entrySet()){
+            if(entity.getValue() == 1){
+                return s.indexOf(entity.getKey()); // l, position in s
+            }
+        }
+        return -1;
     }
 }
