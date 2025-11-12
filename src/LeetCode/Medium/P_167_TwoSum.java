7d2d0091ce9d2068bf85f11cd05956bd4ce07581
ChynaraO <omurzakova_chyna@mail.ru>
2025-11-11 21:56:23 -0600

    Leetcode: 167

---
+++ src/LeetCode/Medium/P_167_TwoSum.java
@@ -0,0 +1,20 @@
+package LeetCode.Medium;
+
+public class P_167_TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        int  left = 0;
+        int right = numbers.length-1;
+        while(left<right){
+            int sum = numbers[left] + numbers[right];
+            if(sum == target){
+                return new int []{++left, ++right};
+            } else if(sum>target){
+                right--;
+            } else{
+                left++;
+            }
+        }
+        return new int[]{-1, -1};
+
+    }
+}
